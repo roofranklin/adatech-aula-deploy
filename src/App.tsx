48b16daaf0080a796cbd698785427ec17e80b6d7
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-04-24 21:34:03 -0300

    Deploy da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { IRegisterData } from './screens/Register/Register.types';
 import { TaskProvider } from "./context/task.context";
 import { AppLayoutContainer, GlobalStyle } from "./styles";
 
-// import Listview from "./screens/Listview";
-// import Register from "./screens/Register";
-
 const LazyRegister = lazy(() => import('./screens/Register'));
 const LazyListview = lazy(() => import('./screens/Listview'));
 
@@ -16,12 +13,6 @@ const App = () => {
     <TaskProvider>
       <GlobalStyle />
       <AppLayoutContainer>
-        {/* <Router>
-          <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/listview" element={<Listview />} />
-          </Routes>
-        </Router> */}
         <Suspense fallback={<div>Carregando...</div>}>
           {/* <Router basename={'/meu-app-react'}> */}
           <Router>
